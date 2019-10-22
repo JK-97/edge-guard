1b62aa9c98aaf964d44674ba1b8de0325d768a7c
Ruihua Liu <liuruihua@jiangxing.ai>
2019-10-22 16:04:01 +0800

    移除 Powermanagement 后台服务

--- management/programmanage/attributes.go
+++ management/programmanage/attributes.go
@@ -97,33 +97,7 @@ stderr_events_enabled=false
 restart_when_binary_changed=true
 `
 
-var Powermanagement = `[program:Powermanagement]
-restart_when_binary_changed=true
-command=/edge/tools/mcutools/powermanagement/bin/powermanagement
-process_name=%(program_name)s
-numprocs=1
-autostart=true
-startsecs=3
-startretries=3
-autorestart=true
-exitcodes=0,2
-stopsignal=TERM
-stopwaitsecs=10
-user=root
-redirect_stderr=false
-stdout_logfile=/edge/logs/%(program_name)s_stdout.log
-stdout_logfile_maxbytes=50MB
-stdout_logfile_backups=10
-stdout_capture_maxbytes=0
-stdout_events_enabled=true
-stderr_logfile=/edge/logs/%(program_name)s_stderr.log
-stderr_logfile_maxbytes=50MB
-stderr_logfile_backups=10
-stderr_capture_maxbytes=0
-stderr_events_enabled=false
-restart_when_binary_changed=true
-`
-
+// Watchdog 与 mcu 通信，防止系统被误杀
 var Watchdog = `[program:WatchDog]
 restart_when_binary_changed=true
 command=/edge/tools/mcutools/watchdog/bin/watchdog
@@ -151,6 +125,7 @@ stderr_events_enabled=false
 
 `
 
+// Mcuserver 与 mcu 通信，获取当前的开机模式
 var Mcuserver = `[program:McuServer]
 restart_when_binary_changed=true
 command=/edge/tools/mcutools/mcuserver/bin/mcuserver
@@ -176,6 +151,8 @@ stderr_logfile_backups=10
 stderr_capture_maxbytes=0
 stderr_events_enabled=false
 `
+
+// Mq 消息队列同步
 var Mq = `[program:Mq]
 restart_when_binary_changed=true
 command=/edge/mnt/mq/bin/mq -c /edge/mnt/mq/bin/mq.cfg
@@ -204,6 +181,8 @@ stderr_logfile_backups=10
 stderr_capture_maxbytes=0
 stderr_events_enabled=false
 `
+
+// Fs 文件系统同步
 var Fs = `[program:Fs]
 restart_when_binary_changed=true
 command=/edge/mnt/fs/bin/fs
@@ -289,16 +268,15 @@ stderr_capture_maxbytes=0
 stderr_events_enabled=false
 `
 var ProgramCfgMap = map[string]string{
-	"Filelistener":    filelistener,
-	"Telegraf":        telegraf,
-	"Db":              Db,
-	"Fs":              Fs,
-	"Mcuserver":       Mcuserver,
-	"Powermanagement": Powermanagement,
-	"Watchdog":        Watchdog,
-	"Mq":              Mq,
-	"Jxserving":       Jxserving,
-	"Cleaner":         Cleaner,
+	"Filelistener": filelistener,
+	"Telegraf":     telegraf,
+	"Db":           Db,
+	"Fs":           Fs,
+	"Mcuserver":    Mcuserver,
+	"Watchdog":     Watchdog,
+	"Mq":           Mq,
+	"Jxserving":    Jxserving,
+	"Cleaner":      Cleaner,
 }
 
 var ProgramSetting = ``
