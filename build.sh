a3d120f7e67bdf5db62f2f221b43f43a10c72f98
shenmingjie <shenmingjie@jiangxingai.com>
2019-10-29 17:33:37 +0800

    加入自动化编译脚本,自动生成version信息

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# 参数:  版本号   编译平台
+# 编译平台缺省时,为当前平台
+set -x
+Version=${1}
+BuildArch=${2}
+# 获取源码最近一次 git commit log，包含 commit sha 值，以及 commit message
+GitCommitLog=`git log --pretty=oneline -n 1`
+# 将 log 原始字符串中的单引号替换成双引号
+GitCommitLog=${GitCommitLog//\'/\"}
+# 检查源码在git commit 基础上，是否有本地修改，且未提交的内容
+GitStatus=`git status -s`
+# 获取当前时间
+BuildTime=`date +'%Y.%m.%d-%H:%M:%S'`
+# 获取 Go 的版本
+# BuildGoVersion=`go version`
+
+
+LDFlags=" \
+    -X 'jxcore/version.GitCommit=${GitCommitLog}' \
+    -X 'jxcore/version.GitStatus=${GitStatus}' \
+    -X 'jxcore/version.BuildDate=${BuildTime}' \
+    -X 'jxcore/version.Version=${Version}' \
+"
+echo ${Version}
+
+if [BuildArch == "arm64" ]; then
+    CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build  -ldflags "$LDFlags" 
+else
+    go build  -ldflags "$LDFlags" 
+    BuildArch=`arch`
+fi
+
+echo "build ${BuildArch} done."
--- version/version.go
+++ version/version.go
@@ -1,24 +1,47 @@
 package version
 
 import (
-    "fmt"
+	"fmt"
     "runtime"
+    "strings"
 )
 
-// GitCommit The git commit that was compiled. This will be filled in by the compiler.
-var GitCommit string
+var (
+	GitCommit = "unknown"
+	Version   = "unknown"
+    BuildDate = "unknown"
+    GitStatus = "unknown"
+	GoVersion = runtime.Version()
+    Type      = Pro
+	OsArch = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
+)
 
-// Version The main version number that is being run at the moment.
-var Version = "1.0.0"
 
-// Type 
-var Type = Pro
 
-// BuildDate 编译日期
-var BuildDate = "2019.08.23"
+// 返回单行格式
+func StringifySingleLine() string {
+	return fmt.Sprintf("GitCommitLog=%s. GitStatus=%s. BuildTime=%s. GoVersion=%s. runtime=%s.",
+    GitCommit, GitStatus, BuildDate, GoVersion, OsArch)
+}
 
-// GoVersion Go version
-var GoVersion = runtime.Version()
+// 返回多行格式
+func StringifyMultiLine() string {
+	return fmt.Sprintf("GitCommitLog=%s\nGitStatus=%s\nBuildTime=%s\nGoVersion=%s\nruntime=%s\n",
+    GitCommit, GitStatus, BuildDate, GoVersion, OsArch)
+}
 
-// OsArch 架构
-var OsArch = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
+// 对一些值做美化处理
+func beauty() {
+	if GitStatus == "" {
+		// GitStatus 为空时，说明本地源码与最近的 commit 记录一致，无修改
+		// 为它赋一个特殊值
+		GitStatus = "cleanly"
+	} else {
+		// 将多行结果合并为一行
+		GitStatus = strings.Replace(strings.Replace(GitStatus, "\r\n", " |", -1), "\n", " |", -1)
+	}
+}
+
+func init() {
+	beauty()
+}
