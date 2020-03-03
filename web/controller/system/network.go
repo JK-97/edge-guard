f213dc57337051a13260b36946d40653f1b1257a
JK-97 <benjamin-dream@outlook.com>
2020-03-03 11:17:52 +0800

    four g api

--- web/controller/system/network.go
+++ web/controller/system/network.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"jxcore/internal/network/dns"
 	"jxcore/internal/network/iface"
+	"jxcore/lowapi/system"
 	"jxcore/web/controller/utils"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -103,4 +105,58 @@ func GetNetworkInterfaceByName(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	utils.RespondSuccessJSON(info, w)
+}
+
+type fourGInfo struct {
+	Enable         bool    `json:"enable"`
+	SignalStrength float64 `json:"signalstrength"`
+	IP             string  `json:"ip"`
+}
+
+func GetFourGInterface(w http.ResponseWriter, r *http.Request) {
+	scpritPath := "/jxbootstrap/worker/scripts/G8100_NoMCU.py"
+	fourGInterface := "usb0"
+	resp := &fourGInfo{}
+
+	iface, err := net.InterfaceByName(fourGInterface)
+	if err != nil {
+		resp.Enable = false
+		utils.RespondSuccessJSON(resp, w)
+		return
+	}
+	ifaceInfo, _ := parseInterfaceInfo(iface)
+
+	output, err := system.RunCommandWithOutput(fmt.Sprintf("python %s CMD AT+CSQ", scpritPath))
+	if err != nil {
+		utils.RespondReasonJSON(resp, w, fmt.Sprintf("exec scprit %s with err,%s", scpritPath, err.Error()), 400)
+		return
+	}
+
+	signalStrengthRssi, err := parseFourGInfo(output)
+	if err != nil {
+		utils.RespondReasonJSON(resp, w, fmt.Sprintf("prase scprit %s output with err,%s", scpritPath, err.Error()), 400)
+		return
+	}
+
+	resp.Enable = true
+	resp.IP = ifaceInfo.IP
+	resp.SignalStrength = 2*signalStrengthRssi - 113
+
+	utils.RespondSuccessJSON(resp, w)
+
+}
+
+func parseFourGInfo(output []byte) (float64, error) {
+	prefix := "+CSQ:"
+	res := ""
+	data := strings.Split(string(output), "\n")
+	for _, line := range data {
+		if strings.HasPrefix(line, prefix) {
+			res = strings.TrimSpace(strings.Trim(line, prefix))
+		}
+	}
+	res = strings.ReplaceAll(res, ",", ".")
+	logger.Info("csq:" + res)
+	return strconv.ParseFloat(res, 64)
+
 }
