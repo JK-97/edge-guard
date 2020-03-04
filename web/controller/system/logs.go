c7715c13f2dee71b25bcab7e524b53e3d00d0930
JK-97 <benjamin-dream@outlook.com>
2020-03-04 14:44:53 +0800

    log system page

--- web/controller/system/logs.go
+++ web/controller/system/logs.go
@@ -1,8 +1,10 @@
 package system
 
 import (
+	"fmt"
 	"jxcore/lowapi/logger"
 	"jxcore/oplog"
+	"jxcore/oplog/types"
 	"jxcore/web/controller/utils"
 	"net/http"
 	"strconv"
@@ -10,6 +12,10 @@ import (
 )
 
 func GetOplog(w http.ResponseWriter, r *http.Request) {
+	type responce struct {
+		Oplogs []types.Oplog `json:"oplogs"`
+		Total  int           `json:"total"`
+	}
 	querys := r.URL.Query()
 	fromStr, ok := querys["from"]
 	if !ok {
@@ -41,15 +47,43 @@ func GetOplog(w http.ResponseWriter, r *http.Request) {
 	logger.Info(untilTime.Format("2006-01-02 15:04:05"), fromTime.Format("2006-01-02 15:04:05"))
 
 	findResult, err := oplog.FindMany(oplog.DefaultTimeFilter(fromTime, untilTime), oplog.DefaultTypeFilter(logMessageType[0]))
-	// logger.Info(findResult)
 	if err != nil {
 		utils.RespondReasonJSON(nil, w, err.Error(), 400)
 		return
 	}
-	utils.RespondSuccessJSON(findResult, w)
+	offset, limit := utils.GetPageInfo(r)
+	length := len(findResult)
+
+	if offset > length {
+		findResult = []types.Oplog{}
+	} else if offset+limit < length {
+		findResult = findResult[offset : offset+limit]
+	}
+
+	resp := &responce{
+		Oplogs: findResult,
+		Total:  length,
+	}
+
+	utils.RespondSuccessJSON(resp, w)
 }
 
 func DownloadOplog(w http.ResponseWriter, r *http.Request) {
 
 	http.ServeFile(w, r, oplog.GetLogFileName())
+}
+
+func ListFilter(offset, limit int, data []types.Oplog) ([]types.Oplog, int, error) {
+
+	res := make([]types.Oplog, 0)
+	length := len(res)
+	if offset > length {
+		return []types.Oplog{}, length, fmt.Errorf("offset more than total")
+	}
+
+	if offset+limit < length {
+		return res[offset : offset+limit], length, nil
+
+	}
+	return res[offset:], length, nil
 }
